c1e37a48b1f41214a1463176330633bc5f70a017
EnCruzUr <cruz_d.3@hotmail.com>
2014-11-29 18:18:42 -0600

    Terminado

--- Proyecto Agenda/Proyecto Agenda/Program.cs
+++ Proyecto Agenda/Proyecto Agenda/Program.cs
@@ -8,22 +8,12 @@ namespace Proyecto_Agenda
 {
     static class Program
     {
-<<<<<<< HEAD
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-=======
->>>>>>> origin/master
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-            Application.Run(new Form1());
-=======
             Application.Run(new Formulario_Visual());
->>>>>>> origin/master
         }
     }
 }
